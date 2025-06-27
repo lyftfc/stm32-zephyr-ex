23b60a521b7b42b38cfecb639a9a26d02a7ae699
Li Yunfan <lyftfc@gmail.com>
2025-06-27 23:53:17 +0800

    Added counter-based (timer) led beacon blinky

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
+// #include <zephyr/drivers/led.h>
+#include <zephyr/drivers/counter.h>
 #include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/class/usbd_hid.h>
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-static const struct gpio_dt_spec led_y = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
-static const struct gpio_dt_spec btn_b0 = GPIO_DT_SPEC_GET(DT_ALIAS(usrbtn), gpios);
 
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
 	     "Console device is not ACM CDC UART device");
@@ -23,14 +24,23 @@ static void error_loop (void)
 	}
 }
 
-void usb_dc_sts_cb (enum usb_dc_status_code cb_status, const uint8_t *param)
+static void usb_dc_sts_cb (enum usb_dc_status_code cb_status, const uint8_t *param)
 {
 	ARG_UNUSED(param);
 	if (cb_status == USB_DC_CONNECTED || cb_status == USB_DC_CONFIGURED)
 		usb_con_ready = true;
 	else if (cb_status != USB_DC_RESUME && cb_status != USB_DC_INTERFACE && cb_status != USB_DC_SOF)
 		usb_con_ready = false;
-	gpio_pin_set_dt(&led_y, usb_con_ready ? 1 : 0);
+	// gpio_pin_set_dt(&led_y, usb_con_ready ? 1 : 0);
+}
+
+static void led_timer_cb (const struct device *dev, uint8_t chan, uint32_t ticks, void *p_usr)
+{
+	printk("Timer interrupt channel %d\n", chan);
+	gpio_pin_toggle_dt(&led);
+	// re-arm the alarm
+	struct counter_alarm_cfg *acfg = (struct counter_alarm_cfg *)p_usr;
+	counter_set_channel_alarm(dev, chan, acfg);
 }
 
 int main(void)
@@ -38,18 +48,31 @@ int main(void)
 	int ret;
 	uint32_t dtr;
 
-	const __unused struct device *const usb_cdc_con = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	const struct device *const usb_cdc_con = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 	uart_line_ctrl_get(usb_cdc_con, UART_LINE_CTRL_DTR, &dtr);
 
 	if (!gpio_is_ready_dt(&led)) { return 0; }
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 	if (ret < 0) { return 0; }
 
-	if (!gpio_is_ready_dt(&led_y)) { return 0; }
-	ret = gpio_pin_configure_dt(&led_y, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) { return 0; }
-	gpio_pin_set_dt(&led_y, 0);
-	if (ret < 0) { return 0; }
+	const struct device *const led_timer = DEVICE_DT_GET(DT_NODELABEL(counter2));
+	if (!device_is_ready(led_timer)) error_loop();
+	struct counter_top_cfg cntop_cfg = {
+		.ticks = 10000, .callback = NULL, .user_data = NULL, .flags = 0
+	};
+	counter_set_top_value(led_timer, &cntop_cfg);
+	counter_start(led_timer);
+
+	struct counter_alarm_cfg alarm_cfg[4];
+	for (int i = 0; i < 4; ++i) {
+        alarm_cfg[i] = (struct counter_alarm_cfg) {
+            .flags = COUNTER_ALARM_CFG_ABSOLUTE,
+            .ticks = (i + 1) * 500,  // 500, 1000, 1500, 2000
+            .callback = led_timer_cb,
+            .user_data = &alarm_cfg[i]
+        };
+        counter_set_channel_alarm(led_timer, i, &alarm_cfg[i]);
+    }
 
 	ret = usb_enable(usb_dc_sts_cb);
 	if (ret < 0) error_loop();
@@ -58,12 +81,6 @@ int main(void)
     {
 		if (usb_con_ready)
 			printk("Hello\n");
-
-		int btn_st = gpio_pin_get_dt(&btn_b0);
-		if (!btn_st) {
-			ret = gpio_pin_toggle_dt(&led);
-			if (ret < 0) { return 0; }
-		}
 
 		k_sleep(K_MSEC(500U));
 	}
